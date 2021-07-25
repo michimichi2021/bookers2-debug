17718deb81309ce0de65eb1fd0c8b8c5b9e9cf59
EC2 Default User <ec2-user@ip-172-31-44-232.ec2.internal>
2021-07-25 11:32:11 +0000

    Finish homework4

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
+  get 'relationships/create'
+  get 'relationships/destroy'
   get 'book_comments/create'
   get 'book_comments/destroy'
   devise_for :users
   root 'homes#top'
   get 'home/about' => 'homes#about'
 
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :users,only: [:show,:index,:edit,:update] do
+    resource :relationships,only: [:create, :destroy]
+    get 'followings' => 'relationships#followings'
+    get 'followers' => 'relationships#followers'
+  end
   
   resources :books do
    resources :book_comments, only: [:create, :destroy]
